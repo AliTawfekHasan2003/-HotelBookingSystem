e5d1616f1b568eb536e114d7172c1d9b5c1bc342
ali <alihasan963003@gmail.com>
2024-10-21 11:57:17 +0300

    Added: localization support for Arabic and English

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,6 +57,7 @@ class Kernel extends HttpKernel
         'role.user' => \App\Http\Middleware\UserRoleMiddleware::class,
         'role.admin' => \App\Http\Middleware\AdminRoleMiddleware::class,
         'role.super_admin' => \App\Http\Middleware\SuperAdminRoleMiddleware::class,
+        'lang' => \App\Http\Middleware\SetLocaleMiddleWare::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
